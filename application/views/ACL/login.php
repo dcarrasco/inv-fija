95ec1acd8590b257f1a958241da87dae7153ddb5
Daniel Carrasco (pc trabajo) <danielcarrasco17@gmail.com>
2014-07-17 16:32:47 -0400

    Agrega viewport en pags login y cambio password

--- application/views/ACL/login.php
+++ application/views/ACL/login.php
@@ -3,6 +3,8 @@
 <head>
 	<title>inventario fija</title>
 	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="icon" href="<?php echo base_url(); ?>favicon.png" type="image/png" />
 
