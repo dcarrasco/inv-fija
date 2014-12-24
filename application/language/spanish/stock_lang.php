d513d077975d38826146e8a0802fe3f8bd2ca6a3
Daniel Carrasco <danielcarrasco17@gmail.com>
2014-12-23 23:49:08 -0300

    Modulo stock

---
+++ application/language/spanish/stock_lang.php
@@ -0,0 +1,60 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$lang['stock_analisis_params']               = 'Parametros consulta';
+$lang['stock_analisis_label_series']         = 'Series';
+$lang['stock_analisis_label_reports']        = 'Reportes';
+$lang['stock_analisis_check_movimientos']    = 'Mostrar movimientos';
+$lang['stock_analisis_check_filtrar_ultmov'] = 'Filtrar ultimo movimiento';
+$lang['stock_analisis_check_despachos']      = 'Mostrar despachos';
+$lang['stock_analisis_check_stock_sap']      = 'Mostrar stock SAP';
+$lang['stock_analisis_check_stock_scl']      = 'Mostrar stock SCL';
+$lang['stock_analisis_check_trafico']        = 'Mostrar trafco';
+$lang['stock_analisis_link_detalle_trafico'] = 'ver detalle trafico';
+$lang['stock_analisis_check_gestor']         = 'Mostrar gestor DTH';
+$lang['stock_analisis_button_query']         = 'Consultar';
+$lang['stock_analisis_button_reset']         = 'Limpiar';
+
+$lang['stock_analisis_title_movimientos'] = 'Movimientos';
+$lang['stock_analisis_title_despachos']   = 'Despachos';
+$lang['stock_analisis_title_stock_sap']   = 'Stock SAP';
+$lang['stock_analisis_title_stock_scl']   = 'Stock SCL';
+$lang['stock_analisis_title_trafico']     = 'Trafico';
+$lang['stock_analisis_title_gestor']      = 'Gestor DTH';
+
+$lang['stock_sap_panel_params']   = 'Parametros consulta';
+$lang['stock_sap_panel_graph']    = 'Ver grafico';
+$lang['stock_sap_label_dates']    = 'Seleccionar Fechas';
+$lang['stock_sap_label_alm']      = 'Seleccionar Almacenes';
+$lang['stock_sap_label_mats']     = 'Seleccionar Detalle Materiales';
+$lang['stock_sap_radio_date1']    = 'Seleccionar ultimo dia mes';
+$lang['stock_sap_radio_date2']    = 'Seleccionar todas las fechas';
+$lang['stock_sap_radio_alm1']     = 'Seleccionar Tipos de Almacen';
+$lang['stock_sap_radio_alm2']     = 'Seleccionar Almacenes';
+$lang['stock_sap_check_show_alm'] = 'Desplegar detalle almacenes';
+$lang['stock_sap_check_mat']      = 'Desplegar detalle materiales';
+$lang['stock_sap_check_lotes']    = 'Desplegar detalle lotes';
+$lang['stock_sap_check_tipstock'] = 'Desplegar detalle tipos stock';
+$lang['stock_sap_radio_equipos']  = 'Equipos';
+$lang['stock_sap_radio_sim']      = 'Simcard';
+$lang['stock_sap_radio_otros']    = 'Otros';
+$lang['stock_sap_label_mostrar']  = 'Mostrar';
+$lang['stock_sap_radio_cant']     = 'cantidad';
+$lang['stock_sap_radio_monto']    = 'monto';
+$lang['stock_sap_button_report']  = 'Reporte';
+$lang['stock_sap_button_export']  = 'Exportar a Excel ...';
+
+$lang['stock_perm_panel_params']  = 'Parametros consulta';
+$lang['stock_perm_panel_date']    = 'Fecha reporte:';
+$lang['stock_perm_label_alm']     = 'Almacenes';
+$lang['stock_perm_label_estados'] = 'Estados stock';
+$lang['stock_perm_label_tipmat']  = 'Tipos material';
+$lang['stock_perm_label_detalle'] = 'Detalle';
+$lang['stock_perm_check_alm']     = 'Mostrar almacenes';
+$lang['stock_perm_check_lotes']   = 'Mostrar lotes';
+$lang['stock_perm_check_modelos'] = 'Mostrar modelos equipos';
+$lang['stock_perm_button_report'] = 'Reporte';
+
+
+/* End of file stock_lang.php */
+/* Location: ./application/language/spanish/stock_lang.php */
