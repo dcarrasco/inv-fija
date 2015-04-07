89efcfa7ce2ea7973109a7d2aff638f003325095
Daniel Carrasco <danielcarrasco17@gmail.com>
2015-04-06 22:59:46 -0300

    Actualiza layout formularios

--- application/views/inventario/imprime_inventario.php
+++ application/views/inventario/imprime_inventario.php
@@ -3,63 +3,74 @@
 </div>
 
 <div class="row">
-	<div class="col-md-10 col-md-offset-1 well">
+	<div class="col-md-8 col-md-offset-2 well">
 		<?php echo form_open('','class="form-horizontal"'); ?>
 		<?php echo form_hidden('formulario','imprime'); ?>
 
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_print_label_inventario'); ?>
-			</label>
-			<div class="col-sm-8">
-				<p class="form-control-static">
-					<?php echo $inventario_id . ' - ' . $inventario_nombre; ?>
-				</p>
-			</div>
-		</div>
+		<fieldset>
+			<legend>Imprimir inventario</legend>
 
-		<div class="form-group <?php echo form_has_error('pag_desde'); ?>">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_print_label_page_from'); ?>
-			</label>
-			<div class="col-sm-8">
-				<?php echo form_input('pag_desde', set_value('pag_desde',1), 'class="form-control"  maxlength="5"'); ?>
-				<?php echo form_error('pag_desde'); ?>
-			</div>
-		</div>
+			<?php if (validation_errors()): ?>
+				<div class="alert alert-danger">
+					<ul>
+						<?php echo validation_errors(); ?>
+					</ul>
+				</div>
+			<?php endif; ?>
 
-		<div class="form-group <?php echo form_has_error('pag_hasta'); ?>">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_print_label_page_to'); ?>
-			</label>
-			<div class="col-sm-8">
-				<?php echo form_input('pag_hasta', set_value('pag_hasta',$max_hoja), 'class="form-control"  maxlength="5"'); ?>
-				<?php echo form_error('pag_hasta'); ?>
-			</div>
-		</div>
-
-		<div class="form-group <?php echo form_has_error('oculta_stock_sap'); ?>">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_print_label_options'); ?>
-			</label>
-			<div class="col-sm-8">
-				<label class="checkbox-inline">
-					<?php echo form_checkbox('oculta_stock_sap', 'oculta_stock_sap', set_checkbox('oculta_stock_sap','oculta_stock_sap', FALSE)); ?>
-					<?php echo $this->lang->line('inventario_print_check_hide_columns'); ?>
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_print_label_inventario'); ?>
 				</label>
+				<div class="col-sm-8">
+					<p class="form-control-static">
+						<?php echo $inventario_id . ' - ' . $inventario_nombre; ?>
+					</p>
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-			</label>
-			<div class="col-sm-8">
-				<button name="submit" type="submit" class="btn btn-primary pull-right" id="btn_imprimir">
-					<span class="glyphicon glyphicon-print"></span>
-					<?php echo $this->lang->line('inventario_print_button_print'); ?>
-				</button>
+			<div class="form-group <?php echo form_has_error('pag_desde') ? 'has-error' : ''; ?>">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_print_label_page_from'); ?>
+				</label>
+				<div class="col-sm-8">
+					<?php echo form_input('pag_desde', set_value('pag_desde',1), 'class="form-control"  maxlength="5"'); ?>
+				</div>
 			</div>
-		</div>
+
+			<div class="form-group <?php echo form_has_error('pag_hasta') ? 'has-error' : ''; ?>">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_print_label_page_to'); ?>
+				</label>
+				<div class="col-sm-8">
+					<?php echo form_input('pag_hasta', set_value('pag_hasta',$max_hoja), 'class="form-control"  maxlength="5"'); ?>
+				</div>
+			</div>
+
+			<div class="form-group <?php echo form_has_error('oculta_stock_sap'); ?>">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_print_label_options'); ?>
+				</label>
+				<div class="col-sm-8">
+					<label class="checkbox-inline">
+						<?php echo form_checkbox('oculta_stock_sap', 'oculta_stock_sap', set_checkbox('oculta_stock_sap','oculta_stock_sap', FALSE)); ?>
+						<?php echo $this->lang->line('inventario_print_check_hide_columns'); ?>
+					</label>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+				</label>
+				<div class="col-sm-8">
+					<button name="submit" type="submit" class="btn btn-primary pull-right" id="btn_imprimir">
+						<span class="glyphicon glyphicon-print"></span>
+						<?php echo $this->lang->line('inventario_print_button_print'); ?>
+					</button>
+				</div>
+			</div>
+
+		</fieldset>
 		<?php echo form_close(); ?>
 	</div>
 </div>
--- application/views/inventario/sube_stock.php
+++ application/views/inventario/sube_stock.php
@@ -3,101 +3,106 @@
 </div>
 
 <div class="row">
-	<div class="col-md-10 col-md-offset-1 well">
+	<div class="col-md-8 col-md-offset-2 well">
 		<?php echo form_open_multipart($this->router->class . '/sube_stock', 'class="form-horizontal" role="form"'); ?>
 		<?php echo form_hidden('formulario','upload'); ?>
 
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_upload_label_inventario'); ?>
-			</label>
-			<div class="col-sm-9">
-				<p class="form-control-static"><?php echo $inventario_id . ' - ' . $inventario_nombre; ?></p>
-			</div>
-		</div>
+		<fieldset>
+			<legend>Subir archivo de stock</legend>
 
-		<?php if (!$show_script_carga): ?>
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_upload_label_file'); ?>
-			</label>
-			<div class="col-sm-9">
-				<div class="alert alert-danger">
-					<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<p><strong>
-						<?php echo $this->lang->line('inventario_upload_warning_line1'); ?>
-					</strong></p>
-					<p>
-						<?php echo $this->lang->line('inventario_upload_warning_line2'); ?>
-						"<?php echo $inventario_nombre?>"
-					</p>
-				</div>
-				<?php echo form_upload('upload_file','','class="form-control"'); ?>
-			</div>
-		</div>
-
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-				<?php echo $this->lang->line('inventario_upload_label_password'); ?>
-			</label>
-			<div class="col-sm-4">
-				<?php echo form_password('password', '', 'class="form-control"'); ?>
-			</div>
-		</div>
-		<?php endif; ?>
-
-		<?php if ($show_script_carga): ?>
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-				Progreso
-			</label>
-			<div class="col-sm-9">
-				<?php echo $upload_error; ?>
-				<?php echo $msj_error; ?>
-				<div id="progreso_carga">
-					<div class="progress">
-						<div class="progress-bar" role="progressbar" style="width: 0%;"></div>
-					</div>
-					<div id="status_progreso1">
-						<?php echo $this->lang->line('inventario_upload_status_OK'); ?>
-						<span id="reg_actual">0</span> / <?php echo ($regs_OK); ?>
-					</div>
-					<div id="status_progreso2">
-						<?php echo $this->lang->line('inventario_upload_status_error'); ?>
-						<span id="reg_error">0</span>
-					</div>
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_upload_label_inventario'); ?>
+				</label>
+				<div class="col-sm-8">
+					<p class="form-control-static"><?php echo $inventario_id . ' - ' . $inventario_nombre; ?></p>
 				</div>
 			</div>
-		</div>
-		<?php endif; ?>
 
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-			</label>
-			<div class="col-sm-9">
-				<?php if ($show_script_carga): ?>
-					<button class="btn btn-primary pull-right" id="ejecuta_carga">
-						<span class="glyphicon glyphicon-play"></span>
-						<?php echo $this->lang->line('inventario_upload_button_load'); ?>
+			<?php if (!$show_script_carga): ?>
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_upload_label_file'); ?>
+				</label>
+				<div class="col-sm-8">
+					<div class="alert alert-danger">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<p><strong>
+							<?php echo $this->lang->line('inventario_upload_warning_line1'); ?>
+						</strong></p>
+						<p>
+							<?php echo $this->lang->line('inventario_upload_warning_line2'); ?>
+							"<?php echo $inventario_nombre?>"
+						</p>
+					</div>
+					<?php echo form_upload('upload_file','','class="form-control"'); ?>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+					<?php echo $this->lang->line('inventario_upload_label_password'); ?>
+				</label>
+				<div class="col-sm-8">
+					<?php echo form_password('password', '', 'class="form-control"'); ?>
+				</div>
+			</div>
+			<?php endif; ?>
+
+			<?php if ($show_script_carga): ?>
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+					Progreso
+				</label>
+				<div class="col-sm-8">
+					<?php echo $upload_error; ?>
+					<?php echo $msj_error; ?>
+					<div id="progreso_carga">
+						<div class="progress">
+							<div class="progress-bar" role="progressbar" style="width: 0%;"></div>
+						</div>
+						<div id="status_progreso1">
+							<?php echo $this->lang->line('inventario_upload_status_OK'); ?>
+							<span id="reg_actual">0</span> / <?php echo ($regs_OK); ?>
+						</div>
+						<div id="status_progreso2">
+							<?php echo $this->lang->line('inventario_upload_status_error'); ?>
+							<span id="reg_error">0</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php endif; ?>
+
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+				</label>
+				<div class="col-sm-8">
+					<?php if ($show_script_carga): ?>
+						<button class="btn btn-primary pull-right" id="ejecuta_carga">
+							<span class="glyphicon glyphicon-play"></span>
+							<?php echo $this->lang->line('inventario_upload_button_load'); ?>
+						</button>
+					<?php else: ?>
+					<button type="submit" name="submit" class="btn btn-primary pull-right" id="btn_guardar">
+						<span class="glyphicon glyphicon-upload"></span>
+						<?php echo $this->lang->line('inventario_upload_button_upload'); ?>
 					</button>
-				<?php else: ?>
-				<button type="submit" name="submit" class="btn btn-primary pull-right" id="btn_guardar">
-					<span class="glyphicon glyphicon-upload"></span>
-					<?php echo $this->lang->line('inventario_upload_button_upload'); ?>
-				</button>
-				<?php endif; ?>
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
 
-		<div class="form-group">
-			<label class="control-label col-sm-3">
-			</label>
-			<div class="col-sm-9">
-				<pre>
+			<div class="form-group">
+				<label class="control-label col-sm-4">
+				</label>
+				<div class="col-sm-8">
+					<pre>
 <?php echo $this->lang->line('inventario_upload_format_file'); ?>
-				</pre>
+					</pre>
+				</div>
 			</div>
-		</div>
+
+		</fieldset>
 		<?php echo form_close(); ?>
 	</div>
 </div>
