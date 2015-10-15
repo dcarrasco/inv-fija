fc59cd848b1b1e760f722752e9d0b8937ce7c830
Daniel Carrasco <danielcarrasco17@gmail.com>
2015-10-14 23:47:54 -0300

    Commit 4

--- application/libraries/grafica_stock.php
+++ application/libraries/grafica_stock.php
@@ -1,20 +1,58 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+/**
+ * INVENTARIO FIJA
+ *
+ * Aplicacion de conciliacion de inventario para la logistica fija.
+ *
+ * @category  CodeIgniter
+ * @package   InventarioFija
+ * @author    Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @copyright 2015 - DCR
+ * @license   MIT License
+ * @link      localhost:1520
+ *
+ */
+if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Clase con functionalidades graficas
+ * *
+ * @category CodeIgniter
+ * @package  InventarioFija
+ * @author   Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @license  MIT License
+ * @link     localhost:1520
+ *
+ */
 class Grafica_stock {
 
+	/**
+	 * Constructor de la clase
+	 *
+	 * @return  void
+	 */
 	public function __construct()
 	{
-
 	}
 
-	public function datos_grafico($stock = array(), $param = array())
+	// --------------------------------------------------------------------
+
+	/**
+	 * Devuelve arreglos con datos en formato para graficar
+	 *
+	 * @param  array $arr_stock Arreglo con datos de stock
+	 * @param  array $arr_param Arreglo con parametreos
+	 * @return array        Datos para construir graficos
+	 */
+	public function datos_grafico($arr_stock = array(), $arr_param = array())
 	{
 		$arr_param_key = array('fecha_ultimodia', 'fecha_todas');
-		foreach ($arr_param_key as $p)
+
+		foreach ($arr_param_key as $parametro)
 		{
-			if (!array_key_exists($p, $param))
+			if ( ! array_key_exists($parametro, $arr_param))
 			{
-				$param[$p] = array();
+				$arr_param[$parametro] = array();
 			}
 		}
 
@@ -27,22 +65,22 @@ class Grafica_stock {
 		$arr_eje_x        = array();
 		$arr_label_series = array();
 
-		foreach($stock as $reg)
+		foreach($arr_stock as $reg)
 		{
 			// si seleccionamos mas de una fecha, entonces usamos la fecha en el eje_X
-			if (count($param['fecha_ultimodia'])>1 OR count($param['fecha_todas'])>1)
+			if (count($arr_param['fecha_ultimodia'])>1 OR count($arr_param['fecha_todas'])>1)
 			{
 				$idx_eje_x = 'fecha_stock';
 				$this->_array_push_unique($arr_eje_x, "'" . $reg[$idx_eje_x] . "'");
 
 				// si seleccionamos tipos de almacen, entonces usamos el tipo de almacen como las series
-				if ($param['sel_tiposalm'] == 'sel_tiposalm')
+				if ($arr_param['sel_tiposalm'] == 'sel_tiposalm')
 				{
 					// si seleccionamos desplegar almacenes, usamos también el almacén como parte de la serie
-					if (array_key_exists('almacen', $param))
+					if (array_key_exists('almacen', $arr_param))
 					{
 						// si hay más de un tipo de almacén, componemos la serie con tipo_alm y almacen
-						if (count($param['tipo_alm']) > 1)
+						if (count($arr_param['tipo_alm']) > 1)
 						{
 							$idx_series = 'cod_almacen';
 							$this->_array_push_unique($arr_label_series, '{label:\'' . $reg['tipo_almacen'] . '/'. $reg[$idx_series] . '-' . $reg['des_almacen'] . '\'}');
@@ -61,7 +99,7 @@ class Grafica_stock {
 						$this->_array_push_unique($arr_label_series, '{label:\'' . $reg[$idx_series] . '\'}');
 					}
 				}
-				else if ($param['sel_tiposalm'] == 'sel_almacenes')
+				else if ($arr_param['sel_tiposalm'] == 'sel_almacenes')
 				{
 					$idx_series = 'cod_almacen';
 					$this->_array_push_unique($arr_label_series, '{label:\'' . $reg['centro'] . '-' . $reg[$idx_series] . ' ' . $reg['des_almacen'] . '\'}');
@@ -74,13 +112,13 @@ class Grafica_stock {
 				$this->_array_push_unique($arr_label_series, '{label:\'' . $reg[$idx_series] . '\'}');
 
 				// si seleccionamos tipos de almacen, entonces usamos el tipo de almacen como las series
-				if ($param['sel_tiposalm'] == 'sel_tiposalm')
+				if ($arr_param['sel_tiposalm'] == 'sel_tiposalm')
 				{
 					// si seleccionamos desplegar almacenes, usamos también el almacén como parte de la serie
-					if ($param['almacen'])
+					if ($arr_param['almacen'])
 					{
 						// si hay más de un tipo de almacén, componemos la serie con tipo_alm y almacen
-						if (count($param['tipo_alm']) > 1)
+						if (count($arr_param['tipo_alm']) > 1)
 						{
 							$idx_eje_x = 'cod_almacen';
 							$this->_array_push_unique($arr_eje_x, '\'' . $reg['tipo_almacen'] . '/'. $reg[$idx_eje_x] . '-' . $reg['des_almacen'] . '\'');
@@ -99,7 +137,7 @@ class Grafica_stock {
 						$this->_array_push_unique($arr_eje_x, '\'' . $reg[$idx_eje_x] . '\'');
 					}
 				}
-				else if ($param['sel_tiposalm'] == 'sel_almacenes')
+				else if ($arr_param['sel_tiposalm'] == 'sel_almacenes')
 				{
 					$idx_eje_x = 'cod_almacen';
 					$this->_array_push_unique($arr_eje_x, '\'' . $reg[$idx_eje_x] . '-' . $reg['des_almacen'] . '\'');
