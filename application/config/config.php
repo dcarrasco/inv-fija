945d25fc8098c8be2beecff70106434d76c22325
Daniel Carrasco (pc trabajo) <danielcarrasco17@gmail.com>
2016-03-24 19:01:20 -0300

    Agrega en base_url discriminación https

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'];
+$config['base_url'] = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
+$config['base_url'] .= '://'.$_SERVER['HTTP_HOST'];
 
 /*
 |--------------------------------------------------------------------------
