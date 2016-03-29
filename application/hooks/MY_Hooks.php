45100541e613b75e47f45c952637f2d4466fe8b4
Daniel Carrasco (pc trabajo) <danielcarrasco17@gmail.com>
2016-03-29 09:43:39 -0300

    Nombre archivos mayuscula (libraries, hooks)

---
+++ application/hooks/MY_Hooks.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * INVENTARIO FIJA
+ *
+ * Aplicacion de conciliacion de inventario para la logistica fija.
+ *
+ * @category  CodeIgniter
+ * @package   InventarioFija
+ * @author    Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @copyright 2015 - DCR
+ * @license   MIT License
+ * @link      localhost:1520
+ *
+ */
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Clase Hook Funcionalidad a ejecutar en todos los controladores
+ * *
+ * @category CodeIgniter
+ * @package  InventarioFija
+ * @author   Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @license  MIT License
+ * @link     localhost:1520
+ *
+ */
+class MY_Hooks {
+
+	/**
+	 * Funciones y operaciones ejecutadas con todos los controladores
+	 *
+	 * @return void
+	 */
+	public function acl_hook()
+	{
+		$ci =& get_instance();
+		$ci->load->model('acl_model');
+		$ci->config->load('inv-fija');
+
+		$whitelist_classes = array('login', 'migration');
+
+		$class = $ci->router->fetch_class();
+
+		$llave_modulo = property_exists($class, 'llave_modulo') ? $ci->llave_modulo : '';
+
+		if ( ! in_array($class, $whitelist_classes) AND  ! $ci->acl_model->autentica_modulo($llave_modulo))
+		{
+			redirect('login/');
+		}
+
+		if (ENVIRONMENT !== 'production')
+		{
+			if ( ! $ci->input->is_ajax_request() AND strpos(uri_string(), 'ajax') === FALSE)
+			{
+				// $ci->output->enable_profiler(TRUE);
+			}
+		}
+
+		// log información
+		log_message('debug', 'REQUEST USER: ' . $ci->session->user . ', URI: '. $ci->uri->uri_string());
+	}
+
+}
---
+++ application/libraries/Grafica_stock.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * INVENTARIO FIJA
+ *
+ * Aplicacion de conciliacion de inventario para la logistica fija.
+ *
+ * @category  CodeIgniter
+ * @package   InventarioFija
+ * @author    Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @copyright 2015 - DCR
+ * @license   MIT License
+ * @link      localhost:1520
+ *
+ */
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Clase con functionalidades graficas
+ * *
+ * @category CodeIgniter
+ * @package  InventarioFija
+ * @author   Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @license  MIT License
+ * @link     localhost:1520
+ *
+ */
+class Grafica_stock {
+
+	/**
+	 * Constructor de la clase
+	 *
+	 * @return  void
+	 */
+	public function __construct()
+	{
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Devuelve arreglos con datos en formato para graficar
+	 *
+	 * @param  array $arr_stock Arreglo con datos de stock
+	 * @param  array $arr_param Arreglo con parametreos
+	 * @return array        Datos para construir graficos
+	 */
+	public function datos_grafico($arr_stock = array(), $arr_param = array())
+	{
+		$arr_param_key = array('fecha_ultimodia', 'fecha_todas');
+
+		foreach ($arr_param_key as $parametro)
+		{
+			if ( ! array_key_exists($parametro, $arr_param))
+			{
+				$arr_param[$parametro] = array();
+			}
+		}
+
+		$graph_q_equipos  = array();
+		$graph_q_simcard  = array();
+		$graph_q_otros    = array();
+		$graph_v_equipos  = array();
+		$graph_v_simcard  = array();
+		$graph_v_otros    = array();
+		$arr_eje_x        = array();
+		$arr_label_series = array();
+
+		foreach($arr_stock as $registro_stock)
+		{
+			// si seleccionamos mas de una fecha, entonces usamos la fecha en el eje_X
+			if (count($arr_param['fecha_ultimodia'])>1 OR count($arr_param['fecha_todas'])>1)
+			{
+				$idx_eje_x = 'fecha_stock';
+				$this->_array_push_unique($arr_eje_x, "'" . $registro_stock[$idx_eje_x] . "'");
+
+				// si seleccionamos tipos de almacen, entonces usamos el tipo de almacen como las series
+				if ($arr_param['sel_tiposalm'] === 'sel_tiposalm')
+				{
+					// si seleccionamos desplegar almacenes, usamos también el almacén como parte de la serie
+					if (array_key_exists('almacen', $arr_param))
+					{
+						// si hay más de un tipo de almacén, componemos la serie con tipo_alm y almacen
+						if (count($arr_param['tipo_alm']) > 1)
+						{
+							$idx_series = 'cod_almacen';
+							$this->_array_push_unique($arr_label_series, '{label:\'' . $registro_stock['tipo_almacen'] . '/'. $registro_stock[$idx_series] . '-' . $registro_stock['des_almacen'] . '\'}');
+						}
+						// si hay solo un tipo de almacén, usamos sólo el codigo de almacen como la serie
+						else
+						{
+							$idx_series = 'cod_almacen';
+							$this->_array_push_unique($arr_label_series, '{label:\'' . $registro_stock[$idx_series] . '-' . $registro_stock['des_almacen'] . '\'}');
+						}
+					}
+					// si no seleccionamos desplegar almacenes, usamos el tipo de almacén como la serie
+					else
+					{
+						$idx_series = 'tipo_almacen';
+						$this->_array_push_unique($arr_label_series, '{label:\'' . $registro_stock[$idx_series] . '\'}');
+					}
+				}
+				else if ($arr_param['sel_tiposalm'] === 'sel_almacenes')
+				{
+					$idx_series = 'cod_almacen';
+					$this->_array_push_unique($arr_label_series, '{label:\'' . $registro_stock['centro'] . '-' . $registro_stock[$idx_series] . ' ' . $registro_stock['des_almacen'] . '\'}');
+				}
+			}
+			// si seleccionamos sólo una fecha
+			else
+			{
+				$idx_series = 'fecha_stock';
+				$this->_array_push_unique($arr_label_series, '{label:\'' . $registro_stock[$idx_series] . '\'}');
+
+				// si seleccionamos tipos de almacen, entonces usamos el tipo de almacen como las series
+				if ($arr_param['sel_tiposalm'] === 'sel_tiposalm')
+				{
+					// si seleccionamos desplegar almacenes, usamos también el almacén como parte de la serie
+					if ($arr_param['almacen'])
+					{
+						// si hay más de un tipo de almacén, componemos la serie con tipo_alm y almacen
+						if (count($arr_param['tipo_alm']) > 1)
+						{
+							$idx_eje_x = 'cod_almacen';
+							$this->_array_push_unique($arr_eje_x, '\'' . $registro_stock['tipo_almacen'] . '/'. $registro_stock[$idx_eje_x] . '-' . $registro_stock['des_almacen'] . '\'');
+						}
+						// si hay solo un tipo de almacén, usamos sólo el codigo de almacen como la serie
+						else
+						{
+							$idx_eje_x = 'cod_almacen';
+							$this->_array_push_unique($arr_eje_x, '\'' . $registro_stock[$idx_eje_x] . '-' . $registro_stock['des_almacen'] . '\'');
+						}
+					}
+					// si no seleccionamos desplegar almacenes, usamos el tipo de almacén para el eje x
+					else
+					{
+						$idx_eje_x = 'tipo_almacen';
+						$this->_array_push_unique($arr_eje_x, '\'' . $registro_stock[$idx_eje_x] . '\'');
+					}
+				}
+				else if ($arr_param['sel_tiposalm'] === 'sel_almacenes')
+				{
+					$idx_eje_x = 'cod_almacen';
+					$this->_array_push_unique($arr_eje_x, '\'' . $registro_stock[$idx_eje_x] . '-' . $registro_stock['des_almacen'] . '\'');
+				}
+			}
+
+			$graph_q_equipos[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['EQUIPOS'];
+			$graph_v_equipos[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['VAL_EQUIPOS']/1000000;
+
+			$graph_q_simcard[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['SIMCARD'];
+			$graph_v_simcard[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['VAL_SIMCARD']/1000000;
+
+			$graph_q_otros[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['OTROS'];
+			$graph_v_otros[$registro_stock[$idx_series]][$registro_stock[$idx_eje_x]] = $registro_stock['VAL_OTROS']/1000000;
+		}
+
+
+		$arr_datos = array(
+			'serie_q_equipos'  => $this->_arr_series_to_string($graph_q_equipos),
+			'serie_v_equipos'  => $this->_arr_series_to_string($graph_v_equipos),
+			'serie_q_simcard'  => $this->_arr_series_to_string($graph_q_simcard),
+			'serie_v_simcard'  => $this->_arr_series_to_string($graph_v_simcard),
+			'serie_q_otros'    => $this->_arr_series_to_string($graph_q_otros),
+			'serie_v_otros'    => $this->_arr_series_to_string($graph_v_otros),
+			'str_eje_x'        => '[' . implode(',', $arr_eje_x) . ']',
+			'str_label_series' => '[' . implode(',', $arr_label_series) . ']',
+		);
+
+		return $arr_datos;
+	}
+
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Ingresa un valor a un arreglo, solo si no existe anteriormente
+	 * @param  array  $arreglo Arreglo a ingresar el valor
+	 * @param  string $valor   Valor a agregar
+	 * @return none
+	 */
+	private function _array_push_unique(&$arreglo, $valor = '')
+	{
+		if ( ! in_array($valor, $arreglo))
+		{
+			array_push($arreglo, $valor);
+		}
+	}
+
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Devuelve un string json con el valor de un arreglo (para usar en javascript)
+	 *
+	 * @param  array $arreglo Arreglo a convertir
+	 * @return string
+	 */
+	private function _arr_series_to_string($arreglo = array())
+	{
+		$arr_temp = array();
+		foreach($arreglo as $arr_elem)
+		{
+			array_push($arr_temp, '[' . implode(',', $arr_elem) . ']');
+		}
+		return('[' . implode(',', $arr_temp) . ']');
+	}
+
+
+
+}
+/* libraries grafica_stock.php */
+/* Location: ./application/libraries/grafica_stock.php */
