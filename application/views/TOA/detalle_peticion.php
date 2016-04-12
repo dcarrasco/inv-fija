8ba676368a21cfaaa28108a05c495c98f8fd37a6
Daniel Carrasco (pc trabajo) <danielcarrasco17@gmail.com>
2016-04-12 12:33:32 -0300

    Agrega trx consumo promedio en peticiones TOA
    Agrega tambien totales en detalle trx

--- application/views/TOA/detalle_peticion.php
+++ application/views/TOA/detalle_peticion.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col-md-10 col-md-offset-1 well">
 
-	<?php $nlinea = 0; ?>
+	<?php $nlinea = 0; $sum_cant = 0; $sum_monto = 0; ?>
 	<?php foreach ($reporte as $linea_detalle): ?>
 
 		<?php if ($nlinea === 0): ?>
@@ -41,51 +41,73 @@
 				</div>
 			</div>
 		</fieldset>
-<hr>
-		<fieldset>
-			<legend>Detalle materiales</legend>
-
-			<table class="table table-striped reporte">
-				<thead>
-					<tr>
-						<th class="text-center">n</th>
-						<th class="text-center">centro</th>
-						<th class="text-center">cod mat</th>
-						<th class="text-left">material</th>
-						<th class="text-center">lote</th>
-						<th class="text-center">valor</th>
-						<th class="text-center">unidad</th>
-						<th class="text-center">cantidad</th>
-						<th class="text-center">monto</th>
-						<th class="text-center">mov SAP</th>
-						<th class="text-center">PEP</th>
-						<th class="text-center">doc SAP</th>
-					</tr>
-				</thead>
-				<tbody>
-		<?php endif; ?>
-
-					<tr>
-						<td class="text-center text-muted"><?php echo $nlinea+1; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['centro']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['material']; ?></td>
-						<td class="text-left"><?php echo $linea_detalle['texto_material']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['lote']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['valor']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['umb']; ?></td>
-						<td class="text-center"><?php echo fmt_cantidad($linea_detalle['cant']); ?></td>
-						<td class="text-center"><?php echo fmt_monto($linea_detalle['monto']); ?></td>
-						<td class="text-center"><?php echo $linea_detalle['codigo_movimiento']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['elemento_pep']; ?></td>
-						<td class="text-center"><?php echo $linea_detalle['documento_material']; ?></td>
-					</tr>
-		<?php $nlinea += 1; ?>
-	<?php endforeach; ?>
-				</tbody>
-			</table>
-
-		</fieldset>
 	</div>
 </div>
+
+<div class="row">
+	<fieldset>
+		<legend>Detalle materiales</legend>
+
+		<table class="table table-striped reporte">
+			<thead>
+				<tr>
+					<th class="text-center">n</th>
+					<th class="text-center">centro</th>
+					<th class="text-center">cod mat</th>
+					<th class="text-left">material</th>
+					<th class="text-center">lote</th>
+					<th class="text-center">valor</th>
+					<th class="text-center">unidad</th>
+					<th class="text-center">cantidad</th>
+					<th class="text-center">monto</th>
+					<th class="text-center">mov SAP</th>
+					<th class="text-center">desc mov SAP</th>
+					<th class="text-center">PEP</th>
+					<th class="text-center">doc SAP</th>
+				</tr>
+			</thead>
+			<tbody>
+		<?php endif; ?>
+
+				<tr>
+					<td class="text-center text-muted"><?php echo $nlinea+1; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['centro']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['material']; ?></td>
+					<td class="text-left"><?php echo $linea_detalle['texto_material']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['lote']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['valor']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['umb']; ?></td>
+					<td class="text-center"><?php echo fmt_cantidad($linea_detalle['cant']); ?></td>
+					<td class="text-center"><?php echo fmt_monto($linea_detalle['monto']); ?></td>
+					<td class="text-center"><?php echo $linea_detalle['codigo_movimiento']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['texto_movimiento']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['elemento_pep']; ?></td>
+					<td class="text-center"><?php echo $linea_detalle['documento_material']; ?></td>
+				</tr>
+		<?php $nlinea += 1; $sum_cant += $linea_detalle['cant']; $sum_monto += $linea_detalle['monto']; ?>
+	<?php endforeach; ?>
+			<tfoot>
+				<tr>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th class="text-center"><?php echo fmt_cantidad($sum_cant) ?></th>
+					<th class="text-center"><?php echo fmt_monto($sum_monto); ?></th>
+					<th></th>
+					<th></th>
+					<th></th>
+					<th></th>
+				</tr>
+			</tfoot>
+			</tbody>
+		</table>
+
+	</fieldset>
+</div>
+
 </div> <!-- fin content-module-main -->
 
