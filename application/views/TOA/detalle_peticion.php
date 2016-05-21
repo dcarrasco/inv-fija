b0556bf688f2dc9adbc08b002e0f4812eeb6c35c
Daniel Carrasco <danielcarrasco17@gmail.com>
2016-05-21 10:53:09 -0400

    Agrega detalle clientes desde TOA

--- application/views/TOA/detalle_peticion.php
+++ application/views/TOA/detalle_peticion.php
@@ -1,13 +1,87 @@
 <div>
-	<div class="col-md-10 col-md-offset-1 well">
-
 	<?php $nlinea = 0; $sum_cant = 0; $sum_monto = 0; ?>
 	<?php foreach ($reporte as $linea_detalle): ?>
+	<?php if ($nlinea === 0): ?>
+	<?php $acoord_x = $linea_detalle['acoord_x']; ?>
+	<?php $acoord_y = $linea_detalle['acoord_y']; ?>
+	<?php $cname    = $linea_detalle['cname']; ?>
 
-		<?php if ($nlinea === 0): ?>
+	<div class="col-md-6 well form-horizontal">
+		<fieldset>
+			<legend>
+				Detalle Cliente
+			</legend>
+
+			<div class="form-group">
+				<label class="control-label col-sm-1">RUT</label>
+				<div class="col-sm-3">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['customer_number']; ?>
+					</p>
+				</div>
+				<label class="control-label col-sm-2">Nombre</label>
+				<div class="col-sm-6">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['cname']; ?>
+					</p>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="control-label col-sm-2">Tel&eacute;fono</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['cphone']; ?>
+					</p>
+				</div>
+				<label class="control-label col-sm-2">Celular</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['ccell']; ?>
+					</p>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="control-label col-sm-2">e-mail</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['cemail']; ?>
+					</p>
+				</div>
+				<label class="control-label col-sm-2">Direcci&oacute;n</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['caddress']; ?>
+					</p>
+				</div>
+			</div>
+
+			<div class="form-group">
+				<label class="control-label col-sm-2">Agencia</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['XA_ORIGINAL_AGENCY']; ?>
+					</p>
+				</div>
+				<label class="control-label col-sm-2">Ciudad</label>
+				<div class="col-sm-4">
+					<p class="form-control-static">
+						<?php echo $linea_detalle['ccity']; ?>
+					</p>
+				</div>
+			</div>
+
+			<div class="form-group">
+			<div id="map_canvas" style="height: 300px"></div>
+			</div>
+
+		</fieldset>
+	</div>
+
+	<div class="col-md-6 well form-horizontal">
 		<fieldset>
 			<legend>Detalle petici&oacute;n</legend>
-
 			<div class="form-group">
 				<label class="control-label col-sm-2">ID Petici&oacute;n</label>
 				<div class="col-sm-4">
@@ -49,6 +123,7 @@
 			</div>
 		</fieldset>
 	</div>
+
 </div>
 
 <div class="col-md-12">
@@ -56,60 +131,78 @@
 		<legend>Detalle materiales</legend>
 	</fieldset>
 
-		<table class="table table-striped table-hover table-condensed reporte">
-			<thead>
-				<tr>
-					<th class="text-center">n</th>
-					<th class="text-center">centro</th>
-					<th class="text-center">cod mat</th>
-					<th class="text-left">material</th>
-					<th class="text-center">lote</th>
-					<th class="text-center">valor</th>
-					<th class="text-center">unidad</th>
-					<th class="text-center">cantidad</th>
-					<th class="text-center">monto</th>
-					<th class="text-center">mov SAP</th>
-					<th class="text-center">desc mov SAP</th>
-					<th class="text-center">PEP</th>
-					<th class="text-center">doc SAP</th>
-				</tr>
-			</thead>
-			<tbody>
+	<table class="table table-striped table-hover table-condensed reporte">
+		<thead>
+			<tr>
+				<th class="text-center">n</th>
+				<th class="text-center">centro</th>
+				<th class="text-center">cod mat</th>
+				<th class="text-left">material</th>
+				<th class="text-center">lote</th>
+				<th class="text-center">valor</th>
+				<th class="text-center">unidad</th>
+				<th class="text-center">cantidad</th>
+				<th class="text-center">monto</th>
+				<th class="text-center">mov SAP</th>
+				<th class="text-center">desc mov SAP</th>
+				<th class="text-center">PEP</th>
+				<th class="text-center">doc SAP</th>
+			</tr>
+		</thead>
+		<tbody>
 		<?php endif; ?>
 
-				<tr>
-					<td class="text-center text-muted"><?php echo $nlinea+1; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['centro']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['material']; ?></td>
-					<td class="text-left"><?php echo $linea_detalle['texto_material']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['lote']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['valor']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['umb']; ?></td>
-					<td class="text-center"><?php echo fmt_cantidad($linea_detalle['cant']); ?></td>
-					<td class="text-center"><?php echo fmt_monto($linea_detalle['monto']); ?></td>
-					<td class="text-center"><?php echo $linea_detalle['codigo_movimiento']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['texto_movimiento']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['elemento_pep']; ?></td>
-					<td class="text-center"><?php echo $linea_detalle['documento_material']; ?></td>
-				</tr>
+			<tr>
+				<td class="text-center text-muted"><?php echo $nlinea+1; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['centro']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['material']; ?></td>
+				<td class="text-left"><?php echo $linea_detalle['texto_material']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['lote']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['valor']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['umb']; ?></td>
+				<td class="text-center"><?php echo fmt_cantidad($linea_detalle['cant']); ?></td>
+				<td class="text-center"><?php echo fmt_monto($linea_detalle['monto']); ?></td>
+				<td class="text-center"><?php echo $linea_detalle['codigo_movimiento']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['texto_movimiento']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['elemento_pep']; ?></td>
+				<td class="text-center"><?php echo $linea_detalle['documento_material']; ?></td>
+			</tr>
 		<?php $nlinea += 1; $sum_cant += $linea_detalle['cant']; $sum_monto += $linea_detalle['monto']; ?>
 	<?php endforeach; ?>
-			<tfoot>
-				<tr>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th class="text-center"><?php echo fmt_cantidad($sum_cant) ?></th>
-					<th class="text-center"><?php echo fmt_monto($sum_monto); ?></th>
-					<th></th>
-					<th></th>
-					<th></th>
-					<th></th>
-				</tr>
-			</tfoot>
-			</tbody>
-		</table>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th class="text-center"><?php echo fmt_cantidad($sum_cant) ?></th>
+				<th class="text-center"><?php echo fmt_monto($sum_monto); ?></th>
+				<th></th>
+				<th></th>
+				<th></th>
+				<th></th>
+			</tr>
+		</tfoot>
+	</table>
+</div>
+
+<script>
+	function initMap() {
+		var ubic = new google.maps.LatLng(<?php echo $acoord_y ?>, <?php echo $acoord_x ?>),
+		    map = new google.maps.Map(document.getElementById('map_canvas'), {
+				center: ubic,
+				zoom: 14
+			}),
+			marker = new google.maps.Marker({
+				position: ubic,
+				title: '<?php echo $cname; ?>'
+			});
+		marker.setMap(map);
+	}
+</script>
+
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&callback=initMap" defer async></script>
