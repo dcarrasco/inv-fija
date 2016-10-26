3a0602b5413d07918193018fc77853d2a4393267
Daniel Carrasco (trabajo) <danielcarrasco17@gmail.com>
2016-10-26 15:30:11 -0300

    Elimina configuraciones no usadas de BD

--- application/config/database.php
+++ application/config/database.php
@@ -71,84 +71,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | the query builder class.
 */
 $active_group = 'default';
-
-if (ENVIRONMENT === 'development')
-{
-	$active_group = 'dcr';
-}
-if (ENVIRONMENT === 'development-mac')
-{
-	$active_group = 'mac-sirio';
-}
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'      => 'dblib:host=sirio;db_name=BD_inventario',
-	'hostname' => 'sirio',
-	'username' => 'invfija',
-	'password' => 'fijainv2014!',
-	'database' => 'BD_inventario',
-	'dbdriver' => 'pdo',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-$db['dcr'] = array(
-	'dsn'	=> 'sqlsrv:Server=127.0.0.1,1500;Database=BD_inventario',
-	'hostname' => 'localhost',
-	'username' => 'invfija',
-	'password' => 'fijainv2014!',
-	'database' => 'BD_inventario',
-	'dbdriver' => 'pdo',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-$db['mac'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'logistica',
-	'password' => 'logistica',
-	'database' => 'logistica',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-$db['mac-sirio'] = array(
 	'dsn'      => 'dblib:host=sirio;db_name=BD_inventario',
 	'hostname' => 'sirio',
 	'username' => 'invfija',
