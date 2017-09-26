6eb3a21900800831f442e6991770667d0f79d2e1
Daniel Carrasco (trabajo) <danielcarrasco17@gmail.com>
2017-09-26 19:34:37 -0300

    Refactor Model_has_persistance

--- application/libraries/Model_has_persistance.php
+++ application/libraries/Model_has_persistance.php
@@ -226,10 +226,6 @@ trait Model_has_persistance {
 	}
 	// --------------------------------------------------------------------
 
-	// =======================================================================
-	// Funciones de interaccion modelo <--> base de datos
-	// =======================================================================
-
 	/**
 	 * Graba el modelo en la base de datos (inserta o modifica, dependiendo si el modelo ya existe)
 	 *
@@ -237,71 +233,159 @@ trait Model_has_persistance {
 	 */
 	public function grabar()
 	{
-		$fields = collect($this->fields);
-
-		$data_where = $fields->only($this->get_campo_id())
-			->map(function($campo, $nombre) { return $this->{$nombre}; })
-			->all();
-
-		$data_update = $fields->except($this->get_campo_id())
-			->filter(function($campo) { return $campo->get_tipo() !== Orm_field::TIPO_HAS_MANY; })
-			->map(function($campo, $nombre) { return $this->{$nombre}; })
-			->all();
-
-		$es_auto_id = ! $fields->only($this->get_campo_id())
-			->filter(function($campo) { return $campo->get_es_autoincrement(); })
-			->is_empty();
-
-		$es_insert = $es_auto_id
-			? empty(collect($data_where)->sum())
-			: $this->db->get_where($this->tabla, $data_where)->row() === NULL OR $this->db->get_where($this->tabla, $data_where)->num_rows() === 0;
-
 		// NUEVO REGISTRO
-		if ($es_insert)
+		if ($this->is_new_record())
 		{
-			if ( ! $es_auto_id)
-			{
-				$this->db->insert($this->tabla, array_merge($data_where, $data_update));
-			}
-			else
-			{
-				$this->db->insert($this->tabla, $data_update);
+			$this->save_new_record();
 
-				$data_where = collect($data_where)
-					->map(function($campo, $campo_id) {
-						$this->values[$campo_id] = $this->db->insert_id();
-						return $this->db->insert_id();
-					})
-					->all();
+			if ($this->has_autoincrement_id())
+			{
+				collect($this->values_id_fields())->each(function($campo, $campo_id) {
+					$this->values[$campo_id] = $this->db->insert_id();
+				});
 			}
 		}
 		// REGISTRO EXISTENTE
 		else
 		{
-			if ( ! empty($data_update))
+			if (! empty($this->values_not_id_fields()))
 			{
-				$this->db->where($data_where);
-				$this->db->update($this->tabla, $data_update);
+				$this->db
+					->where($this->values_id_fields())
+					->update($this->tabla, $this->values_not_id_fields());
 			}
 		}
 
-		$fields->filter(function($campo) { return $campo->get_tipo() === Orm_field::TIPO_HAS_MANY; })
-			->each(function($campo, $nombre_campo) use ($data_where) {
-				$relation = $campo->get_relation();
+		$this->update_pivot_values();
+	}
 
-				$where_delete = collect($relation['id_one_table'])->combine($data_where)->all();
-				$this->db->delete($relation['join_table'], $where_delete);
+	// --------------------------------------------------------------------
+
+	/**
+	 * Recupera arreglo de valores de campos ID
+	 *
+	 * @return array
+	 */
+	protected function values_id_fields()
+	{
+		return collect($this->fields)
+			->only($this->get_campo_id())
+			->map(function($campo, $nombre) { return array_get($this->values, $nombre); })
+			->all();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Recupera arreglo de valores de campos no ID y no HAS_MANY
+	 *
+	 * @return array
+	 */
+	protected function values_not_id_fields()
+	{
+		return collect($this->fields)
+			->except($this->get_campo_id())
+			->filter(function($campo) { return $campo->get_tipo() !== Orm_field::TIPO_HAS_MANY; })
+			->map(function($campo, $nombre) { return array_get($this->values, $nombre); })
+			->all();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Determina si el modelo tiene campos ID autonumericos
+	 *
+	 * @return boolean
+	 */
+	protected function has_autoincrement_id()
+	{
+		return ! collect($this->fields)
+			->only($this->get_campo_id())
+			->filter(function($campo) { return $campo->get_es_autoincrement(); })
+			->is_empty();
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Determina si los campos almacenados en el modelo son nuevos en ls BD
+	 *
+	 * @return boolean
+	 */
+	protected function is_new_record()
+	{
+		$values_id = $this->values_id_fields();
+
+		return $this->has_autoincrement_id()
+			? empty(collect($values_id)->sum())
+			: ($this->db->get_where($this->tabla, $values_id)->row() === NULL
+				OR $this->db->get_where($this->tabla, $values_id)->num_rows() === 0);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Graba (inserta) el modelo en la base de datos
+	 *
+	 * @return boolean
+	 */
+	protected function save_new_record()
+	{
+		$fields = $this->has_autoincrement_id()
+			? $this->values_not_id_fields()
+			: array_merge($this->values_id_fields(), $this->values_not_id_fields());
+
+		return $this->db->insert($this->tabla, $fields);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Actualiza registros de tabla pivote
+	 *
+	 * @return void
+	 */
+	protected function update_pivot_values()
+	{
+		$this->delete_pivot_values();
+
+		collect($this->fields)
+			->filter(function($campo) { return $campo->get_tipo() === Orm_field::TIPO_HAS_MANY; })
+			->each(function($campo, $nombre_campo) {
+				$relation  = $campo->get_relation();
+				$id_fields = collect($relation['id_one_table'])->combine($this->values_id_fields())->all();
 
 				collect($this->{$nombre_campo})
-					->map(function($valor_campo) use ($relation, $where_delete) {
+					->map(function($valor_campo) use ($relation, $id_fields) {
 						return collect($relation['id_many_table'])
 							->combine(explode($this->separador_campos, $valor_campo))
-							->merge($where_delete)
+							->merge($id_fields)
 							->all();
 					})
 					->each(function($insert_values) use ($relation) {
 						$this->db->insert($relation['join_table'], $insert_values);
 					});
+			});
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Borra registros de tabla pivote
+	 *
+	 * @return void
+	 */
+	protected function delete_pivot_values()
+	{
+		$id_fields  = $this->values_id_fields();
+
+		collect($this->fields)
+			->filter(function($campo) { return $campo->get_tipo() === Orm_field::TIPO_HAS_MANY; })
+			->each(function($campo, $nombre_campo) use ($id_fields) {
+				$relation = $campo->get_relation();
+				$where_delete = collect($relation['id_one_table'])->combine($id_fields)->all();
+
+				$this->db->delete($relation['join_table'], $where_delete);
 			});
 	}
 
@@ -322,12 +406,7 @@ trait Model_has_persistance {
 
 		$this->db->delete($this->tabla, $data_where);
 
-		$fields->filter(function($campo) { return $campo->get_tipo() === Orm_field::TIPO_HAS_MANY; })
-			->each(function($campo, $nombre_campo) use ($data_where) {
-				$relation = $campo->get_relation();
-				$where_delete = collect($relation['id_one_table'])->combine($data_where)->all();
-				$this->db->delete($relation['join_table'], $where_delete);
-			});
+		$this->delete_pivot_values();
 	}
 
 }
