b0014c39d852d27a47fef6e514825f8bb5fe21f0
Daniel Carrasco (trabajo) <danielcarrasco17@gmail.com>
2017-10-10 15:48:09 -0300

    Agrega tests orm_field y orm_model

---
+++ application/tests/test_orm_model.php
@@ -0,0 +1,331 @@
+<?php
+
+/**
+ * testeo clase collection
+ *
+ * @category CodeIgniter
+ * @package  InventarioFija
+ * @author   Daniel Carrasco <danielcarrasco17@gmail.com>
+ * @license  MIT License
+ * @link     localhost:1520
+ */
+class test_orm_model extends test_case {
+
+	public function __construct()
+	{
+	    parent::__construct();
+	}
+
+	protected function get_model($config = [])
+	{
+		return new model_test();
+	}
+
+	protected function get_data01()
+	{
+		return [
+			'campo01' => 'value01',
+			'campo02' => 'value02',
+			'campo03' => 3,
+			'campo04' => 1,
+		];
+	}
+
+	public function test_orm_model_get_model_nombre()
+	{
+		$expected = 'model_test';
+		$model = $this->get_model();
+
+		$this->test($model->get_model_nombre(), $expected);
+	}
+
+	public function test_orm_model_get_tabla()
+	{
+		$expected = 'model_tabla';
+		$model = $this->get_model();
+
+		$this->test($model->get_tabla(), $expected);
+	}
+
+	public function test_orm_model_get_label()
+	{
+		$expected = 'model_label';
+		$model = $this->get_model();
+
+		$this->test($model->get_label(), $expected);
+	}
+
+	public function test_orm_model_get_label_plural()
+	{
+		$expected = 'model_label_plural';
+		$model = $this->get_model();
+
+		$this->test($model->get_label_plural(), $expected);
+	}
+
+	public function test_orm_model_get_fields()
+	{
+		$campo01_def = [
+			'label'          => 'label_campo01',
+			'tabla_bd'       => 'model_tabla',
+			'tipo'           => Orm_field::TIPO_CHAR,
+			'largo'          => 10,
+			'texto_ayuda'    => 'texto_ayuda_campo01',
+			'es_id'          => TRUE,
+			'es_obligatorio' => TRUE,
+			'es_unico'       => TRUE
+		];
+
+		$campo02_def = [
+			'label'          => 'label_campo02',
+			'tabla_bd'       => 'model_tabla',
+			'tipo'           => Orm_field::TIPO_CHAR,
+			'largo'          => 10,
+			'texto_ayuda'    => 'texto_ayuda_campo02',
+			'es_id'          => TRUE,
+			'es_obligatorio' => TRUE,
+			'es_unico'       => TRUE
+		];
+
+		$campo03_def = [
+			'label'          => 'label_campo03',
+			'tabla_bd'       => 'model_tabla',
+			'tipo'           => Orm_field::TIPO_INT,
+			'largo'          => 10,
+			'texto_ayuda'    => 'texto_ayuda_campo03',
+		];
+
+		$campo04_def = [
+			'label'          => 'label_campo04',
+			'tabla_bd'       => 'model_tabla',
+			'tipo'           => Orm_field::TIPO_BOOLEAN,
+			'texto_ayuda'    => 'texto_ayuda_campo04',
+		];
+
+		$expected = [
+			'campo01' => (array) new Orm_field('campo01', $campo01_def),
+			'campo02' => (array) new Orm_field('campo02', $campo02_def),
+			'campo03' => (array) new Orm_field('campo03', $campo03_def),
+			'campo04' => (array) new Orm_field('campo04', $campo04_def),
+		];
+		$model = $this->get_model();
+
+		$this->test(collect($model->get_fields())->map(function($campo) {return (array) $campo;})->all(), $expected);
+	}
+
+	public function test_orm_model_get_filtro()
+	{
+		$expected = 'filtro';
+		$model = $this->get_model();
+
+		$model->set_filtro('filtro');
+
+		$this->test($model->get_filtro(), $expected);
+	}
+
+	public function test_orm_model_get_mostrar_lista_campo_existe()
+	{
+		$expected = TRUE;
+		$model = $this->get_model();
+
+		$this->test($model->get_mostrar_lista('campo01'), $expected);
+	}
+
+	public function test_orm_model_get_mostrar_lista_campo_no_existe()
+	{
+		$expected = FALSE;
+		$model = $this->get_model();
+
+		$this->test($model->get_mostrar_lista(), $expected);
+	}
+
+	public function test_orm_model_fill_array()
+	{
+		$expected = $this->get_data01();
+		$model = $this->get_model();
+
+		$this->test($model->fill($this->get_data01())->get_fields_values(), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_campo_existe()
+	{
+		$expected = 'value01';
+		$model = $this->get_model()->fill(['campo01' => 'value01']);
+
+		$this->test($model->campo01, $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_campo_no_existe()
+	{
+		$expected = NULL;
+		$model = $this->get_model()->fill(['campo01' => 'value01']);
+
+		$this->test($model->campo02, $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_campo_id()
+	{
+		$expected = ['campo01', 'campo02'];
+		$model = $this->get_model();
+
+		$this->test($model->get_campo_id(), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_fields_values()
+	{
+		$expected = $this->get_data01();
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_fields_values(), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_id()
+	{
+		$expected = 'value01~value02';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_id(), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_field_value_char()
+	{
+		$expected = 'value01';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_value('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_field_value_integer()
+	{
+		$expected = 3;
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_value('campo03'), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_field_value_boolean_formatted()
+	{
+		$expected = lang('orm_radio_yes');
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_value('campo04'), $expected);
+	}
+
+	public function test_orm_model_has_attributes_get_field_value_boolean_not_formatted()
+	{
+		$expected = 1;
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_value('campo04', FALSE), $expected);
+	}
+
+	public function test_orm_model_has_attributes_fill_from_array()
+	{
+		$expected = $this->get_data01();
+
+		$data01 = $this->get_data01();
+		$data01['campo_xxx'] = 'xxx';
+
+		$model = $this->get_model()->fill_from_array($data01);
+
+		$this->test($model->get_fields_values(), $expected);
+	}
+
+	public function test_orm_model_has_form_form_item()
+	{
+		$expected = 'is_string';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->form_item('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_form_get_field_label()
+	{
+		$expected = 'label_campo01';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_label('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_form_get_field_texto_ayuda()
+	{
+		$expected = 'texto_ayuda_campo01';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_texto_ayuda('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_form_field_es_obligatorio()
+	{
+		$expected = TRUE;
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->field_es_obligatorio('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_form_get_field_marca_obligatorio_TRUE()
+	{
+		$expected = ' <span class="text-danger">*</span>';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_marca_obligatorio('campo01'), $expected);
+	}
+
+	public function test_orm_model_has_form_get_field_marca_obligatorio_FALSE()
+	{
+		$expected = '';
+		$model = $this->get_model()->fill($this->get_data01());
+
+		$this->test($model->get_field_marca_obligatorio('campo03'), $expected);
+	}
+
+
+}
+
+class model_test extends ORM_Model {
+
+	public function __construct($id = NULL)
+	{
+		$this->model_config = [
+			'modelo' => [
+				'tabla'        => 'model_tabla',
+				'label'        => 'model_label',
+				'label_plural' => 'model_label_plural',
+				'order_by'     => 'model_order_by',
+			],
+			'campos' => [
+				'campo01' => [
+					'label'          => 'label_campo01',
+					'tipo'           => Orm_field::TIPO_CHAR,
+					'largo'          => 10,
+					'texto_ayuda'    => 'texto_ayuda_campo01',
+					'es_id'          => TRUE,
+					'es_obligatorio' => TRUE,
+					'es_unico'       => TRUE
+				],
+				'campo02' => [
+					'label'          => 'label_campo02',
+					'tipo'           => Orm_field::TIPO_CHAR,
+					'largo'          => 10,
+					'texto_ayuda'    => 'texto_ayuda_campo02',
+					'es_id'          => TRUE,
+					'es_obligatorio' => TRUE,
+					'es_unico'       => TRUE
+				],
+				'campo03' => [
+					'label'          => 'label_campo03',
+					'tipo'           => Orm_field::TIPO_INT,
+					'largo'          => 10,
+					'texto_ayuda'    => 'texto_ayuda_campo03',
+				],
+				'campo04' => [
+					'label'          => 'label_campo04',
+					'tipo'           => Orm_field::TIPO_BOOLEAN,
+					'texto_ayuda'    => 'texto_ayuda_campo04',
+				],
+			],
+		];
+
+		parent::__construct();
+	}
+}
